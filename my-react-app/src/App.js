94637b9fbd584d59b4c156e3c95993a114ef3225
Zeizil <ekphilbrook@gmail.com>
2025-03-29 11:07:23 -0400

    Added React Environment

---
+++ my-react-app/src/App.js
@@ -0,0 +1,25 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <img id="flowers" src="./public/Images/IMG_3948.PNG" alt="flowers" />
+          <h1>Elizabeth Philbrook</h1>
+          <nav>
+              <h2><a href="#description">About Me</a></h2>
+              <h2><a href="#projects">Work</a></h2>
+              <h2><a href="#contactlinks">Contact</a></h2>
+              <h2><a href="#resumedownload">Resume</a></h2>
+          </nav>
+          <img src="./public/Images/adi-goldstein-EUsVwEOsblE-unsplash.jpg" alt="lit up circuit board" id="circuits" />
+        </div>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
